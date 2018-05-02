7d376915e77b06b8b134fe4627074e99aaa6432f
Christopher Baker <christopher@bitovi.com>
2018-05-02 00:21:36 -0700

    routing

--- app/src/index.js
+++ app/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/styles.css';
 
-import App from './app';
-import { BrowserRouter } from 'react-router-dom';
-
 import { makeInactive, makeActive } from './inactive';
+import { BrowserRouter } from 'react-router-dom';
+import App from './app';
+
+const basename = process.env.NODE_ENV === 'production' ? '/dnd-utils' : '';
 
 makeInactive();
 const container = document.getElementById('root');
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, container, makeActive);
+ReactDOM.render((
+	<BrowserRouter basename={basename}>
+		<App />
+	</BrowserRouter>
+), container, makeActive);
